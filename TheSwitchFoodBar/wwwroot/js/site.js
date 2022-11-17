3c198b49bf17226f36329f8573c9d30a2d1155e8
Jaiye.Gesinde <Jaiye.Gesinde@ISW2-220815-892>
2022-11-17 21:26:34 +0000

    Add project files.

---
+++ TheSwitchFoodBar/wwwroot/js/site.js
@@ -0,0 +1,30 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function OpenPopup(id) {
+    let popup = document.getElementById(id);
+    popup.classList.add("open-popup");
+}
+
+function ClosePopup(id) {
+    let popup = document.getElementById(id);
+    popup.classList.remove("open-popup");
+}
+
+function getFormData(selector) {
+    var data = Object.fromEntries(new FormData(document.querySelector(selector)));
+    console.log(data);
+}
+
+function addSumit(formSelector) {
+    document.querySelector(formSelector).addEventListener("submit", onSubmit);
+}
+
+addSumit("#foodForm");
+
+function onSubmit(e) {
+    console.log("handle submit");
+    e.preventDefault();
+    getFormData("#foodForm");
+}
